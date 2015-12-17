5d2b5f4ca227c0f37ce0ba45f5d6f82ce5180c57
Rouge <sn.falconeye@yahoo.com>
2015-12-16 21:57:04 -0800

    Relabel GUID code

--- Butts/Program.cs
+++ Butts/Program.cs
@@ -12,28 +12,6 @@ namespace Butts
     /// <summary>
     /// The main class.
     /// </summary>
-    public static class G
-    {
-        //GUID class
-        public static string Make()
-        {
-            string g = "";
-            var p = Directory.GetCurrentDirectory() + @"\GUID.butt";
-            //Probably horrible practice, pls user don't touch that file
-            //Maybe write to .dll or .guid or something not .txt
-            if (File.Exists(p))
-            {
-                g = File.ReadAllText(p);
-            }
-            //For sure horrible practice and could be simplified
-            else
-            {
-                g = Guid.NewGuid().ToString();
-                File.WriteAllText(p, g);
-            }
-            return g;
-        }
-    }
     public static class Program
     {
         /// <summary>
@@ -46,6 +24,29 @@ namespace Butts
             Guid g = new Guid(G.Make());
             using (var game = new Game1())
                 game.Run();
+        }
+    }
+    /// <summary>
+    /// GUID class
+    /// </summary>
+    public static class G
+    {
+        public static string Make()
+        {
+            string g = "";
+            string p = Directory.GetCurrentDirectory() + @"\GUID.butt";
+            //Probably horrible practice, pls user don't touch that file
+            if (File.Exists(p))
+            {
+                g = File.ReadAllText(p);
+            }
+            //For sure horrible practice and could be simplified
+            else
+            {
+                g = Guid.NewGuid().ToString();
+                File.WriteAllText(p, g);
+            }
+            return g;
         }
     }
 #endif
