0052aae89a61591e6483a475df1d04c9d8402805
Rouge <sn.falconeye@yahoo.com>
2015-12-23 12:05:00 -0800

    Draws enemies from center, closes #4

--- Butts/Game1.cs
+++ Butts/Game1.cs
@@ -250,7 +250,7 @@ namespace Butts
                     spriteBatch.Draw(_hiAt, Player.hiLocation, null, new Color(255,0,0,5),0F,new Vector2(_hiAt.Width/2,_hiAt.Height/2),1F,SpriteEffects.None,1F);
                 spriteBatch.Draw(_hi, Player.hiLocation, null, Color.Pink, 0F, new Vector2(_hi.Width / 2, _hi.Height / 2), 1F, SpriteEffects.None, 1F);
                 foreach (Enemy en in _enemies)
-                    spriteBatch.Draw(_hi, en.eLoc, en.color);
+                    spriteBatch.Draw(_hi, en.eLoc, null, en.color, 0F, new Vector2(_hi.Width / 2, _hi.Height / 2), 1F, SpriteEffects.None, 0F);
                 _fontRenderer.DrawText(spriteBatch, (int)_fullscreen.X - 100, 50, KeyHandler.timer);
             }
             if (PositionChecker.dead)
